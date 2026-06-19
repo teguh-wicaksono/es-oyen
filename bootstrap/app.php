f0a3769a7f25bda3cf1557bb2b064014eebbf8f6
Chazz <teguhopal111@gmail.com>
2026-06-19 08:37:53 +0700

    Fix: trust Railway proxy so app generates HTTPS URLs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
-
